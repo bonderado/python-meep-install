2dd6316643a0de5906a375b1308839cffecf63e6
Diego Caraffini <diego.caraffini@gmail.com>
2018-03-14 18:10:58 -0400

    Use sudo inside the uninstall script as itis done in the install one.

--- python-meep-remove.sh
+++ python-meep-remove.sh
@@ -3,20 +3,21 @@ MPI=_mpi
 
 # == Uninstall procedure == 
 echo Removing locally compiled meep
-rm /usr/local/bin/meep-mpi
-rm /usr/local/include/meep.hpp
-rm /usr/local/include/meep/mympi.hpp
-rm /usr/local/include/meep/vec.hpp
-rm /usr/local/lib/libmeep${MPI}.a
-rm /usr/local/lib/libmeep${MPI}.la
-rm /usr/local/lib/libmeep${MPI}.so*
-rm /usr/local/lib/pkgconfig/meep${MPI}.pc
-rm /usr/local/share/meep/casimir.scm
-rm /usr/local/share/meep/meep-enums.scm
-rm /usr/local/share/meep/meep.scm
+sudo rm /usr/local/bin/meep-mpi
+sudo rm /usr/local/include/meep.hpp
+sudo rm /usr/local/include/meep/mympi.hpp
+sudo rm /usr/local/include/meep/vec.hpp
+sudo rm /usr/local/lib/libmeep${MPI}.a
+sudo rm /usr/local/lib/libmeep${MPI}.la
+sudo rm /usr/local/lib/libmeep${MPI}.so*
+sudo rm /usr/local/lib/pkgconfig/meep${MPI}.pc
+sudo rm /usr/local/share/meep/casimir.scm
+sudo rm /usr/local/share/meep/meep-enums.scm
+sudo rm /usr/local/share/meep/meep.scm
 echo 'Removing locally compiled python-meep (works for python 2.7)...'
-rm /usr/local/lib/python2.7/dist-packages/_meep${MPI}.so
-rm /usr/local/lib/python2.7/dist-packages/meep${MPI}.py
-rm /usr/local/lib/python2.7/dist-packages/meep${MPI}.pyc
-rm /usr/local/lib/python2.7/dist-packages/python_meep-1.4.egg-info
+sudo rm /usr/local/lib/python2.7/dist-packages/_meep${MPI}.so
+sudo rm /usr/local/lib/python2.7/dist-packages/meep${MPI}.py
+sudo rm /usr/local/lib/python2.7/dist-packages/meep${MPI}.pyc
+sudo rm /usr/local/lib/python2.7/dist-packages/python_meep-1.4.egg-info
+pip uninstall weave
 echo done.
